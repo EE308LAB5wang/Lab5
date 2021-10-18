39e3eb0ec4dde63106a7bfac0d1ea5c24ac007d3
123steve123 <90459362+123steve123@users.noreply.github.com>
2021-10-18 21:07:24 +0800

    Add files via upload

---
+++ Lab5_menu.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<bits/stdc++.h>
+/*
+	功能：显示功能的菜单
+*/
+
+void showMenu()
+{
+	printf("===================================\n");
+	printf("\n欢迎使用小学生四则运算练习软件\n");
+	printf("请输入你的年级（1-6）：\n");
+	printf("\n===================================\n");
+}
+
